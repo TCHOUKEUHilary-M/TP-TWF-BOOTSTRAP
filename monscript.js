75c5de9e08dd02e5850073c7c5fb283f7bd21c91
TCHOUKEUhilary-M <hilarytchoukeu@gmail.com>
2022-08-09 09:41:05 +0200

    envoi en ligne

---
+++ monscript.js
@@ -0,0 +1,11 @@
+/*var details = document.getElementsByClassName('btn-secondary');
+details[0].classlist.replace('btn-secondary', 'btn-warning');
+details[1].classlist.replace('btn-secondary', 'btn-primary');
+details[2].classlist.replace('btn-secondary', 'btn-danger');*/
+alert('bonjour');
+var details = document.getElementsByClassName('btn-secondary');
+details[0].classlist.replace('btn-secondary', 'btn-warning');alert(details[0]);
+details[1].classlist.replace('btn-secondary', 'btn-primary');
+details[2].classlist.replace('btn-secondary', 'btn-danger');
+
+
